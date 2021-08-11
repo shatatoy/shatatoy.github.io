6e3c551ad220805ba6093ba895260e6dcd8084a8
shatatoy <quloff@gmail.com>
2021-08-11 16:13:40 +0300

    чистка

--- help/определить мобильый.js
+++
@@ -1,9 +0,0 @@
-//Определение типа устройства
-var user = detect.parse(navigator.userAgent);
-var deviceType = user.device.type;
-$('body').addClass(deviceType);
-
-// На выходе берем или deviceType или
-// <body class="Mobile">
-// или
-// <body class="Desktop">
